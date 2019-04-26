0fe3aae3b87e0fb821243ec8da6e50e55ba19307
Vivek2402 <49970728+Vivek2402@users.noreply.github.com>
2019-04-26 11:43:52 +0530

    Create tab-menu.js
    comm

---
+++ js/tab-menu.js
@@ -0,0 +1,14 @@
+function openEducation(education, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(education).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+document.getElementById("defaultOpen").click();
